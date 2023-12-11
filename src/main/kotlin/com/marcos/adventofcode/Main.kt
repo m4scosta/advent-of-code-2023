ea493238241821b87eb5af0d5471eadde0b09b88
Marcos Pinto <marcos.pinto@faire.com>
2023-12-11 10:03:12 -0300

    Add day 4 part 2 solution

--- src/main/kotlin/com/marcos/adventofcode/Main.kt
+++ src/main/kotlin/com/marcos/adventofcode/Main.kt
@@ -6,7 +6,7 @@ class Main {
     companion  object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val solution = DayFivePart2()
+            val solution = DayFourPart2()
             for (inputFileName in solution.inputFileNames) {
                 println("Solving $inputFileName")
                 solution.solve(readInputFile(inputFileName))
